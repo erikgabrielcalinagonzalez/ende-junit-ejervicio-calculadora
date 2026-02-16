71561e451ca00d932d9d018e0d133a6cdfdee930
julian <julian@acer.es>
2026-02-16 11:33:14 +0100

    Primera versión

---
+++ README.md
@@ -0,0 +1,19 @@
+# Testing con Junit
+
+Este es un ejemplo sencillo de pruebas unitarias usando Junit 5
+
+Observa que esta aplicación no tiene ninguna clase con el método `main`, no nos hace fatal. Además, tampoco tiene ningún `scanner` ni ningún `print`.
+
+Haz un fork de la aplicación en tu repositorio de Github y contesta a las siguientes preguntas:
+
+1. ¿Qué sentido puede tener este proyecto y para que lo podrías usar?
+2. Revisa las pruebas de la suma y comenta lo que te parezca de interés
+3. Realiza un estudio de caja negra de la división e implementa las pruebas en junit: Se realizará en markdown.
+
+
+
+## Instrucciones
+
+El alumno deberá hacer un fork de este proyecto, poner el repositorio como **privado**, agregar al profesor como colaborador (julparper) e implementar la solución solicitada (preguntas y código).
+
+>Se deberá utilizar este fichero, y los artefactos de código del proyecto, para resolver el ejercicio.
---
+++ src/test/java/es/etg/dax/testing/CalculadoraTest.java
@@ -0,0 +1,59 @@
+package es.etg.dax.testing;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import es.etg.dax.testing.exception.OperacionNoValidaException;
+
+// Las clases de test suelen tener el sufijo Test
+public class CalculadoraTest {
+
+    Calculadora calculadora = new Calculadora(); //
+
+    @Test
+    void sumarPositivos() {
+
+        int valor1 = 2;
+        int valor2 = 3;
+        int esperado = 5;
+
+        assertEquals(esperado, Calculadora.sumar(valor1, valor2));
+    }
+
+    @Test
+    void sumarPositivosMal() {
+
+        int valor1 = 2;
+        int valor2 = 3;
+        int esperado = 4;
+
+        assertEquals(esperado, Calculadora.sumar(valor1, valor2));
+    }
+
+    // @Test de varias pruebas juntas
+    @Test
+    void sumar() {
+
+        // Se realizan 4 pruebas y si las cuatro son ok, sumar será ok.
+        assertAll("Suma",
+                () -> assertEquals(5, Calculadora.sumar(1, 4), "1 + 4 = 5"),
+                () -> assertEquals(5, Calculadora.sumar(2, 3), "2 + 3 = 5"),
+                () -> assertEquals(1, Calculadora.sumar(0, 1), "0 + 1 = 1"),
+                () -> assertEquals(-1, Calculadora.sumar(1, -2), "1 + (-2) = -1"));
+
+    }
+
+    // Esta prueba comprueba que al dividir por cero se lanza una excepción
+    // controlada: OperacionNoValidaException
+    @Test
+    @DisplayName("Probar la división por cero")
+    void dividirPorZeroException() {
+        var ex = assertThrows(OperacionNoValidaException.class, () -> Calculadora.dividir(4, 0),
+                "La división por cero no está permitida");
+        assertEquals(OperacionNoValidaException.MSG, ex.getMessage());
+
+    }
+}
